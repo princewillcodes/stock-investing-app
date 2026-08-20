678a7b2b225dcc1e6641db92bf107498b0ba4b0b
princewillcodes <boatengw9@gmail.com>
2026-08-20 09:58:15 +0000

    feat: add Convex client provider

---
+++ app/ConvexClientProvider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ReactNode, useMemo } from "react";
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  const client = useMemo(() => {
+    const url = process.env.NEXT_PUBLIC_CONVEX_URL;
+    return url ? new ConvexReactClient(url) : null;
+  }, []);
+  return client ? <ConvexProvider client={client}>{children}</ConvexProvider> : <>{children}</>;
+}
